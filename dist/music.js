9326cd0abdca8fb6c1c5a1a800b6e3425441f666
LAPTOP-UQLEA11L\阿伟的Computer <2773073542@qq.com>
2023-06-16 15:54:10 +0800

    Site updated: 2023-06-16 15:54:10

--- dist/music.js
+++ dist/music.js
@@ -10,13 +10,13 @@ const ap = new APlayer({
             name: '我记得',
             artist: '赵雷',
             url: 'https://www.ytmp3.cn/down/78713.mp3',
-            cover: 'cover1.jpg',
+            cover: '\dist\2.jpg',
         },
         {
             name: '向云端',
             artist: '小霞/海洋BO',
             url: 'http://music.163.com/song/media/outer/url?id=2049512697.mp3',
-            cover: 'cover2.jpg',
+            cover: '\dist\1.jpg',
         }
     ]
 });
