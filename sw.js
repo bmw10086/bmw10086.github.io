dcbf1c5ffeb1e0196b0af4911567741737b0bb40
LAPTOP-UQLEA11L\阿伟的Computer <2773073542@qq.com>
2023-09-06 22:25:02 +0800

    Site updated: 2023-09-06 22:25:02

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","6eddb97c021c802679e8e9b21f2e65eb"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
